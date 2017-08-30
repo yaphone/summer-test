57d55aea93330481c74085fdb85ac79210bc4ba5
yaphone <zhouyafeng2013@gmail.com>
2017-08-30 23:46:03 +0800

    日常开发
    日常开发

---
+++ src/main/java/cn/zhouyafeng/summerstudy/controller/ViewController.java
@@ -0,0 +1,15 @@
+package cn.zhouyafeng.summerstudy.controller;
+
+import cn.zhouyafeng.summer.framework.annotation.Action;
+import cn.zhouyafeng.summer.framework.annotation.Controller;
+import cn.zhouyafeng.summer.framework.bean.Param;
+import cn.zhouyafeng.summer.framework.bean.View;
+
+@Controller
+public class ViewController {
+
+    @Action("/customer")
+    public View index(Param param){
+        return new View("customer.jsp");
+    }
+}
